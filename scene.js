285092310dfe23a16ccfee2693c8914850bbc629
Zachary Wedel <zwede710@gmail.com>
2025-01-30 18:49:40 -0600

    playing around with class file, redundant for rn but I want a way to make objects with this library

--- scene.js
+++ scene.js
@@ -5,7 +5,7 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color("#0d97ff");
 
 const camera = new THREE.PerspectiveCamera(
-  110,
+  90,
   window.innerWidth / window.innerHeight,
   0.2,
   2000
@@ -19,12 +19,22 @@ const renderer = new THREE.WebGLRenderer();
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.98);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.95);
 scene.add(ambientLight);
 
-const light = new THREE.DirectionalLight( 0xff0000, 1 );
-light.position.set( 0, 5, 5 );
-light.castShadow = true;
+//Create a DirectionalLight and turn on shadows for the light
+const light = new THREE.DirectionalLight( 0xffffff, 1 );
+light.position.set( 0, 1, 1 ); //default; light shining from top
+light.visible = true;
+light.castShadow = true; // default false
+
+
+//Set up shadow properties for the light
+light.shadow.mapSize.width = 512; // default
+light.shadow.mapSize.height = 512; // default
+light.shadow.camera.near = 0.1; // default
+light.shadow.camera.far = 10; // default
+
 scene.add( light );
 
 console.log(light);
@@ -37,7 +47,7 @@ document.body.appendChild(renderer.domElement);
 //ground
 const ground = new THREE.Mesh(
   new THREE.PlaneGeometry(20, 20, 32, 32),
-  new THREE.MeshPhongMaterial({
+  new THREE.MeshStandardMaterial({
     color: 0x228B22,
     side: THREE.DoubleSide,
   })
@@ -52,7 +62,18 @@ scene.add(ground);
 renderer.setAnimationLoop(() => renderer.render(scene, camera));
 camera.position.z = 4;
 
+const sphereGeometry = new THREE.SphereGeometry( 1.3, 32, 32 );
+const sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
+const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+sphere.castShadow = true; //default is false
+sphere.receiveShadow = false; //default
+
+sphere.position.y = 2.5
+
+scene.add( sphere );
+
 //first object
-const cube1 = new cube(scene, camera, renderer, 1, 1, 0, 2, 0xD2122E, false);
-cube1.renderCube();
+
+
+renderer.render(scene, camera);
 
--- shapes/Cube.js
+++ shapes/Cube.js
@@ -1,7 +1,17 @@
 import * as THREE from "three";
 
 class cube {
-  constructor(scene, camera, renderer, width, height,xPos,yPos, color, isWiremesh) {
+  constructor(
+    scene,
+    camera,
+    renderer,
+    width,
+    height,
+    xPos,
+    yPos,
+    color,
+    isWiremesh
+  ) {
     this.color = color;
     this.scene = scene;
     this.camera = camera;
@@ -15,26 +25,25 @@ class cube {
 
   renderCube() {
     const material = new THREE.MeshPhongMaterial({
-      color: this.color,
-      wireframe: this.isWiremesh,
+      color: this.color
     });
     const geometry = new THREE.BoxGeometry(this.width, this.height, 1, 1, 1);
     const cubeRendered = new THREE.Mesh(geometry, material);
-    cubeRendered.castShadow = true;
-    cubeRendered.receiveShadow = false;
+    
     cubeRendered.position.set(this.xPos, this.yPos, 1);
-
-    console.log(cubeRendered);
 
     const _animateCube = () => {
       cubeRendered.rotation.x += 0.005;
       cubeRendered.rotation.y += 0.005;
-      this.renderer.render(this.scene, this.camera);
+      //this.renderer.render(this.scene, this.camera);
     };
 
+    console.log(cubeRendered)
+
+    cubeRendered.castShadow = true;
+    cubeRendered.receiveShadow = false;
     this.scene.add(cubeRendered);
     this.renderer.setAnimationLoop(_animateCube);
-    
   }
 }
 
